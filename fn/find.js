7d34458380eecc0e515ffb7dbe1c85c95fbd3a0c
ginpei <ginpei@gmail.com>
2015-12-11 17:39:48 -0800

    allow filtering selector to start with child selector

--- fn/find.js
+++ fn/find.js
@@ -1,9 +1,15 @@
 gQuery.fn.find = function(selector) {
-	var els = [];
-	this.forEach(function(el) {
-		gQuery(selector, el).forEach(function(el) {
-			els.push(el);
+	var trimmed = String.trim(selector);
+	if (trimmed.charAt(0) === '>') {
+		return this.children(trimmed.slice(1));
+	}
+	else {
+		var els = [];
+		this.forEach(function(el) {
+			gQuery(selector, el).forEach(function(el) {
+				els.push(el);
+			});
 		});
-	});
-	return gQuery(els);
+		return gQuery(els);
+	}
 };
--- test/find.js
+++ test/find.js
@@ -27,4 +27,28 @@ describe('find()', function() {
 			expect($el).not.toBe($elRoot);
 		});
 	});
+
+	describe('starting with child selector', function() {
+		var $el, $elRoot;
+		beforeEach(function() {
+			$elRoot = gQuery(test_helper.elem(
+				'<div>' +
+					'<div id="child1">' +
+						'<div id="child1_1"></div>' +
+					'</div>' +
+					'<div id="child2"></div>' +
+				'</div>' +
+				'<div>' +
+					'<div id="child3"></div>' +
+				'</div>'));
+			$el = $elRoot.find('>div');
+		});
+
+		it('finds descendant elements', function() {
+			expect($el.length).toBe(3);
+			expect($el[0].id).toBe('child1');
+			expect($el[1].id).toBe('child2');
+			expect($el[2].id).toBe('child3');
+		});
+	});
 });
