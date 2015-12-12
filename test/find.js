129c4ad32dbcec1435717dae301e0425a4097274
ginpei <ginpei@gmail.com>
2015-12-11 17:37:26 -0800

    wrap existing tests in a description section

--- test/find.js
+++ test/find.js
@@ -1,28 +1,30 @@
 describe('find()', function() {
-	var $el, $elRoot;
-	beforeEach(function() {
-		$elRoot = gQuery(test_helper.elem(
-			'<div>' +
-				'<div id="child1">' +
-					'<div id="child1_1"></div>' +
+	describe('with normal selector', function() {
+		var $el, $elRoot;
+		beforeEach(function() {
+			$elRoot = gQuery(test_helper.elem(
+				'<div>' +
+					'<div id="child1">' +
+						'<div id="child1_1"></div>' +
+					'</div>' +
+					'<div id="child2"></div>' +
 				'</div>' +
-				'<div id="child2"></div>' +
-			'</div>' +
-			'<div>' +
-				'<div id="child3"></div>' +
-			'</div>'));
-		$el = $elRoot.find('div');
-	});
+				'<div>' +
+					'<div id="child3"></div>' +
+				'</div>'));
+			$el = $elRoot.find('div');
+		});
 
-	it('finds descendant elements', function() {
-		expect($el.length).toBe(4);
-		expect($el[0].id).toBe('child1');
-		expect($el[1].id).toBe('child1_1');
-		expect($el[2].id).toBe('child2');
-		expect($el[3].id).toBe('child3');
-	});
+		it('finds descendant elements', function() {
+			expect($el.length).toBe(4);
+			expect($el[0].id).toBe('child1');
+			expect($el[1].id).toBe('child1_1');
+			expect($el[2].id).toBe('child2');
+			expect($el[3].id).toBe('child3');
+		});
 
-	it('returns new instance', function() {
-		expect($el).not.toBe($elRoot);
+		it('returns new instance', function() {
+			expect($el).not.toBe($elRoot);
+		});
 	});
 });
